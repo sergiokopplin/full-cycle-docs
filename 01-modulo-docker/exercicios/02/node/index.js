1509ee0e008d36f4d72fb830b4f9128a1c951c87
Sergio Kopplin <sergiokopplin@gmail.com>
2022-03-18 23:42:13 -0300

    feat: 🎸 docker node exercise

--- 01-modulo-docker/exercicios/02/node/index.js
+++ 01-modulo-docker/exercicios/02/node/index.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const faker = require('faker')
 const app = express()
-const port = 3000
+const port = process.env.APP_PORT || 3000
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
 
 app.get('/', (_, res) => {
-  res.send('<h1>Full Cycle Rocks!</h1>');
+  const name = faker.name.findName()
+
+  connection.query(`INSERT INTO people (nome) VALUES ('${name}')`)
+  connection.query(`SELECT nome FROM people`, (_, results, _) => {
+    res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${!!results.length ? results.map(el => `<li>${el.nome}</li>`).join('') : ''}
+      </ol>
+    `)
+  })
 })
 
 app.listen(port, () => {
-  console.log('rodando na', port)
+  console.log('Up on:', port);
 })
