49cf91c8cc5f0d09d5b5cd139b0d95cca668ede2
Sergio Kopplin <sergiokopplin@gmail.com>
2022-03-18 22:08:12 -0300

    feat: 🎸 exercicio docker node

--- 01-modulo-docker/exercicios/02/node/index.js
+++ 01-modulo-docker/exercicios/02/node/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Kopplin');
+app.get('/', (_, res) => {
+  res.send('<h1>Full Cycle Rocks!</h1>');
 })
 
 app.listen(port, () => {
-    console.log('rodando na porta: ', port)
+  console.log('rodando na', port)
 })
