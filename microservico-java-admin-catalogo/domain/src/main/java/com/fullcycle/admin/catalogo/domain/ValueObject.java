9fc4870246bdc93559b4ee68692e01e66fbd5add
Sergio Kopplin <sergiokopplin@gmail.com>
2022-04-27 23:09:15 -0300

    feat: java category aggregateroot

---
+++ microservico-java-admin-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/ValueObject.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.domain;
+
+public abstract class ValueObject {
+  
+}
--- microservico-java-admin-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
+++ microservico-java-admin-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -1,10 +1,8 @@
 package com.fullcycle.admin.catalogo.domain.category;
-
+import com.fullcycle.admin.catalogo.domain.AggregateRoot;
 import java.time.Instant;
-import java.util.UUID;
 
-public class Category {
-  private String id;
+public class Category extends AggregateRoot<CategoryID> {
   private String name;
   private String description;
   private boolean active;
@@ -13,21 +11,21 @@ public class Category {
   private Instant deletedAt;
 
   private Category(
-    final String id,
-    final String name,
-    final String description,
-    final boolean active,
-    final Instant createdAt,
-    final Instant updatedAt,
-    final Instant deletedAt
+    final CategoryID anId,
+    final String aName,
+    final String aDescription,
+    final boolean isActive,
+    final Instant aCreationDate,
+    final Instant anUpdateDate,
+    final Instant aDeletedDate
   ) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.active = active;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.deletedAt = deletedAt;
+    super(anId);
+    this.name = aName;
+    this.description = aDescription;
+    this.active = isActive;
+    this.createdAt = aCreationDate;
+    this.updatedAt = anUpdateDate;
+    this.deletedAt = aDeletedDate;
   }
   
   public static Category newCategory(
@@ -35,17 +33,13 @@ public class Category {
     final String aDescription,
     final boolean aIsActive
   ) {
-    final var id = UUID.randomUUID().toString();
+    final var id = CategoryID.unique();
     final var now = Instant.now();
     return new Category(id, aName, aDescription, aIsActive, now, now, null);  
   }
 
-  public String getId() {
+  public CategoryID getId() {
     return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
   }
 
   public String getName() {
