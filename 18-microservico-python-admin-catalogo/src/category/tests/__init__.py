32b091d5957ec3d8e44475d53d1752a13b7945df
Sergio Kopplin <sergiokopplin@gmail.com>
2022-05-31 18:18:53 -0300

    fix: deletes project

--- 18-microservico-python-admin-catalogo/src/category/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import unittest
-from category.domain.entities import Category
-
-
-class TestCategory(unittest.TestCase):
-
-    def test_constructor(self):
-        Category('Movie', 'Description', True, datetime.now())
