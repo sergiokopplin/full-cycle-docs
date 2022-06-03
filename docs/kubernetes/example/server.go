9d0270a82d9368dd8a64db5c23452c9b93c48615
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-02 22:16:45 -0300

    docs: k8s liveness

--- docs/kubernetes/example/server.go
+++ docs/kubernetes/example/server.go
@@ -3,16 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
-	http.HandleFunc("/", Hello)
+	http.HandleFunc("/healthz", Healthz)
 	http.ListenAndServe(":80", nil)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	age := os.Getenv("AGE")
-	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
